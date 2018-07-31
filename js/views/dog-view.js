5f287636056153770b9e628a69437b63ba1db65f
tganyan <tganyan@gmail.com>
2018-07-31 13:36:30 -0700

    adding search functionality, index page init, handlebars tempaltes

---
+++ js/views/dog-view.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var app = app || {};
+
+(function(module) {
+    var puppyView = {};
+
+    puppyView.initIndexPage = () => {
+        let puppyList = app.Puppy.all;
+        app.mobileNav();
+        $('#featured-view').empty();
+        app.showOnly('#home');
+        for (var i = 0; i < 2; i++) {
+            $('#featured-view').append(puppyList[Math.random() * puppyList.length].toHtml('featured-puppy-template'));
+        };    
+    };
+
+
+
+
+    // TYLER - This is a potential function for breeder puppy details; STRETCH GOAL
+    // puppyView.initPuppyDetail = context => {
+    //     let singlePuppy = new app.Puppy(context);
+    //     $('#puppy-detail').empty();
+    //     app.showOnly('#puppy-detail');
+    //     $('#puppy-detail').append(singlePuppy.toHtml('puppy-detail-template'));
+    // }
+
+    puppyView.initSearchForm = () => {
+        app.showOnly('.search-view');
+
+        $('#puppy-search').on('submit', function(event) {
+            event.preventDefault();
+
+            let puppy = {
+                location: event.target.zipcode.value || '',
+            }
+
+            module.Puppy.find(puppy, initSearchResultsPage);
+
+            event.target.zipcode.value = '';
+        });
+    };
+
+    puppyView.initSearchResultsPage = () => {
+        app.showOnly('#search-results');
+        $('#search-list').empty();
+
+        module.Puppy.all.forEach(puppy => $('#search-list').append(puppy.toHtml()));
+    }
+
+    module.puppyView = puppyView;
+})(app);
