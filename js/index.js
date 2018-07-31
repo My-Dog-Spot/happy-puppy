6eba7702e48cd6b9f43ea06957c1bd3b306bc3ce
tganyan <tganyan@gmail.com>
2018-07-31 09:52:23 -0700

    refactoring index.js, moving js files into mvc structure

--- js/index.js
+++ js/index.js
@@ -1,59 +1,90 @@
-// Create variables for the location in the DOM where the images will go
-var featureLeftDivEl = document.getElementById('featured-left-div');
-var featureRightDivEl = document.getElementById('featured-right-div');
+// // Create variables for the location in the DOM where the images will go
+// var featureLeftDivEl = $('#featured-left-div');
+// var featureRightDivEl = $('#featured-right-div');
 
-// Specify the feature badge to overlay on the featured dog.
-var featureBadgePath = "img/Uncyclopedia_Featured.png";
 
-// Variable for manipulating image size
-var featuredImageHeight = 300;
-var featuredImageWidth = 400;
+// // TYLER - Commenting out featured badge stuff, no longer needed
+// // // Specify the feature badge to overlay on the featured dog.
+// // var featureBadgePath = "img/Uncyclopedia_Featured.png";
 
-function renderFeaturedDogs() {
+// // // Variable for manipulating image size
+// // var featuredImageHeight = 300;
+// // var featuredImageWidth = 400;
 
-  // Retrieve the portrait for the featured dogs and size them appropriately
-  // left image
-  var leftFeaturedImgEl = document.createElement('img');
-  leftFeaturedImgEl.id = 'featured-left';
-  leftFeaturedImgEl.src = dog[featDogs[0]].portrait;
-  leftFeaturedImgEl.height = featuredImageHeight;
-  leftFeaturedImgEl.width = featuredImageWidth;
-  featureLeftDivEl.appendChild(leftFeaturedImgEl);
+// function renderFeaturedDogs() {
 
-  // feature badge overlay on left image
-  var leftFeaturedImgBadgeEl = document.createElement('img');
-  leftFeaturedImgBadgeEl.id = 'badge';
-  leftFeaturedImgBadgeEl.src = featureBadgePath;
-  featureLeftDivEl.appendChild(leftFeaturedImgBadgeEl);
+//   // Retrieve the portrait for the featured dogs and size them appropriately
+//   // left image
+//   var leftFeaturedImgEl = document.createElement('img');
+//   leftFeaturedImgEl.id = 'featured-left';
+//   leftFeaturedImgEl.src = dog[featDogs[0]].portrait;
+//   // TYLER - Image sizing should generally be handled by css
+//   // leftFeaturedImgEl.height = featuredImageHeight;
+//   // leftFeaturedImgEl.width = featuredImageWidth;
+//   featureLeftDivEl.appendChild(leftFeaturedImgEl);
 
-  // overlay the dogs litter name
-  var leftFeaturedDogNameEl = document.createElement('p');
-  leftFeaturedDogNameEl.id = 'name-tag';
-  leftFeaturedDogNameEl.textContent = dog[featDogs[0]].name;
-  featureLeftDivEl.appendChild(leftFeaturedDogNameEl);
+//   // feature badge overlay on left image
+//   var leftFeaturedImgBadgeEl = document.createElement('img');
+//   leftFeaturedImgBadgeEl.id = 'badge';
+//   leftFeaturedImgBadgeEl.src = featureBadgePath;
+//   featureLeftDivEl.appendChild(leftFeaturedImgBadgeEl);
 
-  // right image
-  var rightFeaturedImgEl = document.createElement('img');
-  rightFeaturedImgEl.id = 'featured-right';
-  rightFeaturedImgEl.src = dog[featDogs[1]].portrait;
-  rightFeaturedImgEl.height = featuredImageHeight;
-  rightFeaturedImgEl.width = featuredImageWidth;
-  featureRightDivEl.appendChild(rightFeaturedImgEl);
+//   // overlay the dogs litter name
+//   var leftFeaturedDogNameEl = document.createElement('p');
+//   leftFeaturedDogNameEl.id = 'name-tag';
+//   leftFeaturedDogNameEl.textContent = dog[featDogs[0]].name;
+//   featureLeftDivEl.appendChild(leftFeaturedDogNameEl);
 
-  // feature badge overlay on right image
-  var rightFeaturedImgBadgeEl = document.createElement('img');
-  rightFeaturedImgBadgeEl.id = 'badge';
-  rightFeaturedImgBadgeEl.src = featureBadgePath;
-  featureRightDivEl.appendChild(rightFeaturedImgBadgeEl);
+//   // right image
+//   var rightFeaturedImgEl = document.createElement('img');
+//   rightFeaturedImgEl.id = 'featured-right';
+//   rightFeaturedImgEl.src = dog[featDogs[1]].portrait;
+//   rightFeaturedImgEl.height = featuredImageHeight;
+//   rightFeaturedImgEl.width = featuredImageWidth;
+//   featureRightDivEl.appendChild(rightFeaturedImgEl);
 
-  // overlay the dogs litter name
-  var rightFeaturedDogNameEl = document.createElement('p');
-  rightFeaturedDogNameEl.id = 'name-tag';
-  rightFeaturedDogNameEl.textContent = dog[featDogs[1]].name;
-  featureRightDivEl.appendChild(rightFeaturedDogNameEl);
-}
+//   // feature badge overlay on right image
+//   var rightFeaturedImgBadgeEl = document.createElement('img');
+//   rightFeaturedImgBadgeEl.id = 'badge';
+//   rightFeaturedImgBadgeEl.src = featureBadgePath;
+//   featureRightDivEl.appendChild(rightFeaturedImgBadgeEl);
 
-renderFeaturedDogs();
+//   // overlay the dogs litter name
+//   var rightFeaturedDogNameEl = document.createElement('p');
+//   rightFeaturedDogNameEl.id = 'name-tag';
+//   rightFeaturedDogNameEl.textContent = dog[featDogs[1]].name;
+//   featureRightDivEl.appendChild(rightFeaturedDogNameEl);
+// }
+
+// renderFeaturedDogs();
+
+
+'use strict';
+
+var app = app || {};
+
+(function(module){
+
+  let productionApiUrl = 'insert cloud API server URL here';
+  let developmentApiUrl = 'http://localhost:3000';
+
+  module.isProduction= /^(?!localhost|127)/.test(window.location.hostname);
+
+  module.ENVIRONMENT = {
+    apiUrl: module.isProduction ? productionApiUrl : developmentApiUrl
+  };
+
+  module.mobileNav = () => {
+    $('#menu').on('click', () => {
+      if ($('nav').hasClass('flexed')) {
+        $('nav').removeClass('flexed');
+      } else {
+        $('nav').addClass('flexed');
+      }
+    });
+  }
+
+})(app);
 
 
 
