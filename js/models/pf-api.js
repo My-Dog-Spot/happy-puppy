721376795fd26b51b3acaa225b0225ba2d64804e
J Christie <jchristie1@gmail.com>
2018-08-01 12:07:55 -0700

    Tested API call methods

--- js/models/pf-api.js
+++ js/models/pf-api.js
@@ -1,19 +1,43 @@
 'use strict';
 
-let data = {};
+// let data = {};
 
-var api =function() {
+// var api =function() {
+//   $.ajax({
+//     // type:GET,
+//     url:'http://api.petfinder.com/pet.find?key=7d4d266f8529bb64239eba2ebbd5eba2&format=json&location=98121&animal=dog',
+//     dataType: 'json',
+//     success: function (jsonData) {
+//     }
+//   })
+//     .then(jsonData =>{ data = jsonData.petfinder.pets.pet;});
+
+// };
+
+// api();
+
+// console.log(data);
+
+
+// var api =function(func) {
+//   $.ajax({
+//     url:'http://api.petfinder.com/pet.find?key=7d4d266f8529bb64239eba2ebbd5eba2&format=json&location=98121&animal=dog',
+//     dataType: 'json',
+//     success: function (jsonData) {
+//     }
+//   })
+//     .then(jsonData => func( jsonData ));
+
+// };
+
+// api(function(json){...});
+
+let api = ( ) => {
   $.ajax({
-    // type:GET,
     url:'http://api.petfinder.com/pet.find?key=7d4d266f8529bb64239eba2ebbd5eba2&format=json&location=98121&animal=dog',
-    dataType: 'json',
-    success: function (jsonData) {
-    }
+    dataType: 'json'
   })
-    .then(jsonData =>{ data = jsonData.petfinder.pets.pet;});
-
+    .then(jsonData =>{ return jsonData});
 };
 
-api();
-
-console.log(data);
+api().then((json) => {...})
