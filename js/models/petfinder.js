9dc4da090c4f54f89abef31ce918d0f0e16531e6
Hoffit <dehull_1@yahoo.com>
2018-08-02 14:41:26 -0700

    Got the location map for each shelter puppy from a google static map api call and rendered with the search results.

--- js/models/petfinder.js
+++ js/models/petfinder.js
@@ -4,6 +4,7 @@ var app = app || {};
 
 (function(module){
 
+  // TODO Refactor so that ShelterPuppy object has association to this Address object.
   function Address(rawAddressObj) {
       this.city = rawAddressObj.city.$t;
       this.state = rawAddressObj.state.$t;
@@ -14,31 +15,30 @@ var app = app || {};
     this.age = rawPuppyObj.age.$t;
     this.gender = rawPuppyObj.sex.$t;
     this.story = rawPuppyObj.description.$t;
-    this.contactLocation = rawPuppyObj.contact.city.$t;
+    this.city = rawPuppyObj.contact.city.$t;
+    this.state = rawPuppyObj.contact.state.$t;
     this.image_url = rawPuppyObj.media.photos.photo[3].$t;
+    this.locationMap = `https://maps.googleapis.com/maps/api/staticmap?center=${this.city}${this.state}&zoom=14&size=400x400`;
   }
 
   ShelterPuppy.all = [];
 
   ShelterPuppy.prototype.toHtml = function(templateId) {
-    return app.render(templateId, this);
+    return module.render(templateId, this);
   };
 
   ShelterPuppy.loadAll = rows => {
       console.log(rows);
       ShelterPuppy.all = rows.map(puppyRawData => new ShelterPuppy(puppyRawData));
-  }
+  };
 
-  ShelterPuppy.find = (zipcode, callback) => 
-    $.get(`http://api.petfinder.com/pet.find?key=7d4d266f8529bb64239eba2ebbd5eba2&format=json&location=${zipcode}&animal=dog`)  
-    .then((data) => data.petfinder.pets.pet)
-    .then(ShelterPuppy.loadAll)
-    .then(callback)
-    .catch(console.error);
+  ShelterPuppy.find = (postalCode, callback) => {
+    $.get(`http://api.petfinder.com/pet.find?key=7d4d266f8529bb64239eba2ebbd5eba2&format=json&location=${postalCode}&animal=dog`)
+      .then((data) => data.petfinder.pets.pet)
+      .then(ShelterPuppy.loadAll)
+      .then(callback)
+      .catch(console.error);
+  };
 
   module.ShelterPuppy = ShelterPuppy;
 })(app);
-
-
-
-
